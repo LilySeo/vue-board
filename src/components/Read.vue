6ebbb70f22abdb10288bf7607758513a72f5c41d
lilyseo <purism02@gmail.com>
2020-04-10 11:23:39 +0900

    R: read 읽어들이기 작업 -  data를 로컬에 만들기 - v-for써서 불러오기

---
+++ src/components/Read.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>번호</th>
+          <th>제목</th>
+          <th>내용</th>
+          <th>글쓴이</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(data, index) in datas" :key="index">
+          <td>{{ index + 1 }}</td>
+          <td>{{ data.title }}</td>
+          <td>{{ data.content }} </td>
+          <td>{{ data.wirter }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import data from "@/data"
+
+export default {
+  name: 'Read',
+  data() {
+    return {
+      datas: data
+    }
+  }
+}
+</script>
+
+<style>
+table td {
+  border: 1px solid #ddd;
+}
+</style>
---
+++ src/data/index.js
@@ -0,0 +1,17 @@
+export default[
+  {
+    title: "엄마",
+    content: "대니의 와이프이자 헨리의 엄마",
+    writer: "영희"
+  },
+  {
+    title: "아빠",
+    content: "영희의 남편이자 헨리의 아빠",
+    writer: "대니"
+  },
+  {
+    title: "the 헨리",
+    content: "대니와 영희의 아들",
+    writer: "헨리"
+  },
+]
